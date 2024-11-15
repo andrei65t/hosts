597291d693793f82ef28c2e3e6b7171c5af69c11
Andrei Popescu-Ilioniu <andrei65t@gmail.com>
2024-11-15 13:11:09 +0200

    commit2

--- lab.sh
+++ lab.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-# Verifica validitatea adreselor de IP din /etc/hosts
-for linie in $(cat /etc/hosts); do
-  ip=$(echo "$linie" | cut -d' ' -f2)
+# Definirea funcției pentru verificarea validității unei adrese de IP
+verifica_ip() {
+  local nume_host=$1
+  local ip=$2
+  local server_dns=$3
+
+  # Verifica dacă adresa de IP este validă
   if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Adresa de IP $ip este validă."
+    # Verifica asocierea folosind serverul DNS
+    if dig +short $nume_host @$server_dns | grep -q $ip; then
+      echo "Adresa de IP $ip pentru hostul $nume_host este validă și asociată cu serverul DNS $server_dns."
+    else
+      echo "Adresa de IP $ip pentru hostul $nume_host nu este asociată cu serverul DNS $server_dns."
+    fi
   else
     echo "Adresa de IP $ip nu este validă."
   fi
-done
+}
+
+# Exemplu de utilizare a funcției
+verifica_ip "$1" "$2" "$3"
